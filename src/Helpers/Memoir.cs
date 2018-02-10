5cccac723a044e8534816d3f76c49e129c1b4511
Samrit Taechawongtham <samrit@central.tech>
2018-02-10 09:45:19 +0700

    Improve Memoir to handle thread-safe issue.

--- src/Helpers/Memoir.cs
+++ src/Helpers/Memoir.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using RZ.Foundation.Extensions;
 using static RZ.Foundation.Prelude;
@@ -8,23 +10,102 @@ namespace RZ.Foundation.Helpers
 {
     public static class Memoir
     {
-        public static Func<A, B> From<A, B>(Func<A, B> loader, Func<A, Option<B>> cache, Action<A, B> store) => x => cache(x).Get(Identity, () => SideEffect<B>(result => store(x, result))(loader(x)));
+        public interface ICache<in A,B>
+        {
+            Option<B> Get(A x);
+            void Store(A x, B result);
+        }
 
-        public static Func<A, Task<B>> FromAsync<A, B>(Func<A, Task<B>> loader, Func<A, Task<Option<B>>> cache, Func<A, B, Task> store) => async x => {
-            var v = await cache(x);
+        public interface ICacheAsync<A, B>
+        {
+            Task<Option<B>> GetAsync(A x);
+            Task StoreAsync(A x, B result);
+        }
+
+        public interface ILocker<A>
+        {
+            bool TryEnter(A x);
+            void Wait(A x);
+            void Leave(A x);
+        }
+        public sealed class DictionaryCache<A, B> : ICache<A, B>
+        {
+            readonly Dictionary<A,B> cache = new Dictionary<A, B>();
+            public Option<B> Get(A x) => cache.Get(x);
+            public void Store(A x, B result) => cache[x] = result;
+        }
+        public sealed class MultithreadLocker<A> : ILocker<A> {
+            sealed class Locker
+            {
+                public int Lock;
+            }
+            readonly ConcurrentDictionary<A,Locker> lockers = new ConcurrentDictionary<A, Locker>();
+
+            public bool TryEnter(A x) {
+                var @lock = lockers.GetOrAdd(x, _ => new Locker());
+                return Interlocked.Increment(ref @lock.Lock) == 1;
+            }
+
+            public void Wait(A x) {
+                var @lock = lockers[x];
+                while (@lock.Lock != 0) { Thread.Yield(); }
+            }
+
+            public void Leave(A x) => Interlocked.Exchange(ref lockers[x].Lock, 0);
+        }
+
+        static B ExecuteMemoir<A,B>(Func<A,B> loader, ICache<A,B> cache, A x) =>
+            cache.Get(x).Get(Identity, () => SideEffect<B>(result => cache.Store(x, result))(loader(x)));
+
+        public static Func<A, B> From<A, B>(Func<A, B> loader, ICache<A, B> cache) => x => ExecuteMemoir(loader, cache, x);
+
+        public static Func<A, B> From<A, B>(Func<A, B> loader, ICache<A,B> cache, ILocker<A> locker) => x => {
+retry:
+            var v = cache.Get(x);
+            if (v.IsSome)
+                return v.Get();
+            if (locker.TryEnter(x))
+                try {
+                    return ExecuteMemoir(loader, cache, x);
+                }
+                finally {
+                    locker.Leave(x);
+                }
+            else {
+                locker.Wait(x);
+                goto retry;
+            }
+        };
+
+        public static async Task<B> ExecuteMemoirAsync<A, B>(Func<A, Task<B>> loader, ICacheAsync<A, B> cache, A x) {
+            var v = await cache.GetAsync(x);
             if (v.IsSome)
                 return v.Get();
             var result = await loader(x);
-            await store(x, result);
+            await cache.StoreAsync(x, result);
             return result;
+        }
+
+        public static Func<A, Task<B>> FromAsync<A, B>(Func<A, Task<B>> loader, ICacheAsync<A,B> cache) => x => ExecuteMemoirAsync(loader, cache, x);
+
+        public static Func<A, Task<B>> FromAsync<A, B>(Func<A, Task<B>> loader, ICacheAsync<A,B> cache, ILocker<A> locker) => async x => {
+retry:
+            var v = await cache.GetAsync(x);
+            if (v.IsSome)
+                return v.Get();
+            if (locker.TryEnter(x))
+                try {
+                    return await ExecuteMemoirAsync(loader, cache, x);
+                }
+                finally {
+                    locker.Leave(x);
+                }
+            else {
+                locker.Wait(x);
+                goto retry;
+            }
         };
 
-        public static Func<A, B> DictWith<A, B>(Func<A, B> loader) {
-            var cache = new Dictionary<A,B>();
-            return From( loader
-                       , y => cache.Get(y)
-                       , (a, b) => cache[a] = b
-                       );
-        }
+        public static Func<A, B> DictWith<A, B>(Func<A, B> loader) => From(loader, new DictionaryCache<A,B>());
     }
 }
--- test/MemoirTest.cs
+++ test/MemoirTest.cs
@@ -1,4 +1,7 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
 using RZ.Foundation.Helpers;
 using Xunit;
 
@@ -20,6 +23,30 @@ namespace RZ.Foundation
 
             result.Should().NotBe(3, "sideeffect should be increased once only!");
             result.Should().Be(2);
+        }
+
+        [Fact]
+        public void MemoirDictSameKeyMultithread() {
+            var sideeffect = 0;
+            int test(int x) {
+                ++sideeffect;
+                return x + sideeffect;
+            }
+            var memoir = Memoir.From(test, new Memoir.DictionaryCache<int,int>(), new Memoir.MultithreadLocker<int>());
+            var startLine = new ManualResetEventSlim();
+            var tasks = Enumerable.Range(1, 1000).Select(_ => Task.Run(() => {
+                startLine.Wait();
+                return memoir(1);
+            })).ToArray();
+
+            startLine.Set();    // go!
+
+            Task.WaitAll(tasks.Cast<Task>().ToArray());
+
+            var results = tasks.Select(t => t.Result).ToArray();
+
+            results.All(r => r == 2).Should().BeTrue();
+            sideeffect.Should().Be(1, "sideeffect should be called just once!");
         }
     }
 }
