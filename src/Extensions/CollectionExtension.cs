59d3846fcebfcd4c0e967a791859a4f8d682a1d1
Samrit Taechawongtham <samrit@central.tech>
2018-02-19 16:21:41 +0700

    Add Choose and Partition for collection.

--- src/Extensions/CollectionExtension.cs
+++ src/Extensions/CollectionExtension.cs
@@ -21,11 +21,36 @@ namespace RZ.Foundation.Extensions {
         public static T[] RemoveAt<T>(this IEnumerable<T> array, int n) => array.Take(n).Skip(n + 1).ToArray();
         public static Option<T> Get<TKey, T>(this Dictionary<TKey, T> dict, TKey key) => dict.TryGetValue(key, out var result) ? result : Option<T>.None();
 
-        public static Option<T> TryFirst<T>(this IEnumerable<T> seq, Func<T, bool> predicate){
-            foreach (var item in seq.Where(predicate))
-                return item;
+        public static IEnumerable<B> Choose<A, B>(this IEnumerable<A> array, Func<A, Option<B>> chooser) =>
+            from i in array
+            let opt = chooser(i)
+            where opt.IsSome
+            select opt.Get();
+
+        public static IEnumerable<B> Choose<A, B>(this IEnumerable<A> array, Func<A, int, Option<B>> chooser) {
+            var count = 0;
+            foreach (var i in array) {
+                var opt = chooser(i, count++);
+                if (opt.IsSome)
+                    yield return opt.Get();
+            }
+        }
+
+        public static (T[], T[]) Partition<T>(this IEnumerable<T> e, Func<T, bool> partitioner) {
+            var trueResult = new List<T>();
+            var falseResult = new List<T>();
+            foreach(var i in e)
+                (partitioner(i)? trueResult : falseResult).Add(i);
+            return (trueResult.ToArray(), falseResult.ToArray());
+        }
+
+        public static Option<T> TryFirst<T>(this IEnumerable<T> seq) {
+            foreach (var item in seq) return item;
             return Option<T>.None();
         }
+
+        public static Option<T> TryFirst<T>(this IEnumerable<T> seq, Func<T, bool> predicate) => seq.Where(predicate).TryFirst();
+
         public static IEnumerable<IEnumerable<T>> Batch<T>(this IEnumerable<T> data, int size) {
             using(var itor = data.GetEnumerator())
                 while (itor.MoveNext())
