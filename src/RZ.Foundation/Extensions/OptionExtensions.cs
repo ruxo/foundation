84190d37d0015339882553f7eafdb835bd702acb
Ruxo Zheng <rz@just.sent.as>
2023-07-06 19:22:44 +0700

    Add ToNullable reference version back

--- src/RZ.Foundation/Extensions/OptionExtensions.cs
+++ src/RZ.Foundation/Extensions/OptionExtensions.cs
@@ -23,9 +23,13 @@ namespace RZ.Foundation.Extensions
             return o.IsSome;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> ToOption<T>(this T data) => data;
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> ToOption<T>(this T? data) where T : struct => data.HasValue? Some(data.Value) : Option<T>.None;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<ImmutableHashSet<T>> ToNotEmptyOption<T>(this ImmutableHashSet<T> data) =>
             data.IsEmpty ? LanguageExt.Prelude.None : data;
 
@@ -33,28 +37,42 @@ namespace RZ.Foundation.Extensions
             var array = data.AsArray();
             return array.Length > 0 ? array : LanguageExt.Prelude.None;
         }
-
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> None<T>() => Option<T>.None;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Result<T> ToResult<T>(this Option<T> o) => o.IsSome ? o.Get().AsSuccess() : DummyException.AsFailure<T>();
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Result<T> ToResult<T>(this Option<T> o, Func<Exception> none) => o.IsSome ? o.Get().AsSuccess() : none().AsFailure<T>();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> Join<T>(this Option<Option<T>> doubleOption) => doubleOption.Bind(i => i);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> Call<A, B, T>(this Option<(A, B)> x, Func<A, B, T> f) => x.Map(p => p.CallFrom(f));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> Call<A, B, C, T>(this Option<(A, B, C)> x, Func<A, B, C, T> f) => x.Map(p => p.CallFrom(f));
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<Unit> Call<A, B>(this Option<(A, B)> x, Action<A, B> f) => x.Map(p => p.CallFrom(f));
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<Unit> Call<A, B, C>(this Option<(A, B, C)> x, Action<A, B, C> f) => x.Map(p => p.CallFrom(f));
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<Option<TR>> Chain<T, TR>(this Task<Option<T>> t, Func<T, Option<TR>> mapper) => (await t).Bind(mapper);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<Option<TR>> ChainAsync<T, TR>(this Task<Option<T>> t, Func<T, Task<Option<TR>>> mapper) =>
             await (await t).ToAsync().BindAsync(async x => (await mapper(x)).ToAsync()).ToOption();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<T> Get<T>(this Task<Option<T>> t) => (await t).Get();
         
         [Obsolete("use Match instead")]
         public static Task<TR> Get<T, TR>(this Task<Option<T>> t, Func<T, TR> someMapper, Func<TR> noneMapper) => t.Match(someMapper, noneMapper);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<TR> Match<T, TR>(this Task<Option<T>> t, Func<T, TR> someMapper, Func<TR> noneMapper) =>
             (await t).Match(someMapper, noneMapper);
 
@@ -62,6 +80,7 @@ namespace RZ.Foundation.Extensions
         public static Task<TR> GetAsync<T, TR>(this Task<Option<T>> t, Func<T, Task<TR>> someMapper, Func<Task<TR>> noneMapper) =>
             t.MatchAsync(someMapper, noneMapper);
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<TR> MatchAsync<T, TR>(this Task<Option<T>> t, Func<T, Task<TR>> someMapper, Func<Task<TR>> noneMapper) =>
             await (await t).MatchAsync(someMapper, noneMapper);
 
@@ -71,15 +90,23 @@ namespace RZ.Foundation.Extensions
         [Obsolete("Use Option<T>.ToNullable extension method")]
         public static async Task<T> GetOrDefault<T>(this Task<Option<T>> t) => (await t).GetOrDefault()!;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<T> GetOrElse<T>(this Task<Option<T>> t, T noneValue) => (await t).IfNone(noneValue);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<T> GetOrElse<T>(this Task<Option<T>> t, Func<T> noneValue) => (await t).IfNone(noneValue);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static async Task<T> GetOrElseAsync<T>(this Task<Option<T>> t, Func<Task<T>> noneValue) => await (await t).IfNoneAsync(noneValue);
+       
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T? ToNullable<T>(this Option<T> opt) where T : class => opt.IfNoneUnsafe((T?)default); 
     }
 
     public static class OptionExtensions
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T Get<T>(this Option<T> opt) => opt.GetOrThrow(() => new InvalidOperationException());
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static B Get<A,B>(this Option<A> opt, Func<A,B> getter) => opt.IsSome? getter(opt.Get()) : throw new InvalidOperationException();
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -96,8 +123,10 @@ namespace RZ.Foundation.Extensions
             return opt.Get();
         }
 
+        [Obsolete("Use `ToNullable` instead")]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static T? GetOrDefault<T>(this Option<T> opt) => opt.IfNoneUnsafe(() => default!);
+        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static B? GetOrDefault<A,B>(this Option<A> opt, Func<A,B> getter) => opt.Map(getter).IfNoneUnsafe(() => default!);
 
@@ -108,6 +137,7 @@ namespace RZ.Foundation.Extensions
         /// <param name="elseValue">Value to replace if none</param>
         /// <typeparam name="T">type parameter for opt</typeparam>
         /// <returns>Result option type</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> OrElse<T>(this Option<T> opt, T elseValue) => opt.IsSome? opt : Some(elseValue);
 
         /// <summary>
@@ -117,6 +147,7 @@ namespace RZ.Foundation.Extensions
         /// <param name="elseValue">Value to replace if none</param>
         /// <typeparam name="T">type parameter for opt</typeparam>
         /// <returns>Result option type</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> OrElse<T>(this Option<T> opt, Option<T> elseValue) => opt.IsSome? opt : elseValue;
 
         /// <summary>
@@ -126,6 +157,7 @@ namespace RZ.Foundation.Extensions
         /// <param name="elseFunc">A replace function to evaluate if none</param>
         /// <typeparam name="T">type parameter for opt</typeparam>
         /// <returns>Result option type</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<T> OrElse<T>(this Option<T> opt, Func<Option<T>> elseFunc) => opt.IsSome? opt : elseFunc();
 
 #if NETSTANDARD2_1
@@ -147,6 +179,7 @@ namespace RZ.Foundation.Extensions
         /// <param name="elseFunc">A replace function to evaluate if none</param>
         /// <typeparam name="T">type parameter for opt</typeparam>
         /// <returns>Result option type</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Task<Option<T>> OrElseT<T>(this Option<T> opt, Func<Task<Option<T>>> elseFunc) =>
             opt.IsSome ? Task.FromResult(opt) : elseFunc();
 
@@ -157,6 +190,7 @@ namespace RZ.Foundation.Extensions
         /// <param name="elseFunc">A replace function to evaluate if none</param>
         /// <typeparam name="T">type parameter for opt</typeparam>
         /// <returns>Result option type</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static ValueTask<Option<T>> OrElseT<T>(this Option<T> opt, Func<ValueTask<Option<T>>> elseFunc) =>
             opt.IsSome ? ValueTask.FromResult(opt) : elseFunc();
 
@@ -171,6 +205,7 @@ namespace RZ.Foundation.Extensions
         public static Task<Option<T>> OrElseAsync<T>(this Option<T> opt, Func<Task<Option<T>>> elseFunc) =>
             opt.IsSome ? Task.FromResult(opt) : elseFunc();
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Option<R> TryCast<T,R>(this Option<T> opt) =>
             opt.IsSome && opt.Get() is R v ? Some(v) : None;
     }
@@ -186,7 +221,7 @@ namespace RZ.Foundation.Extensions
         public OptionSerializable(Option<T> opt)
         {
             HasValue = opt.IsSome;
-            Value = opt.GetOrDefault();
+            Value = opt.IfNoneUnsafe((T?)default);
         }
         public Option<T> ToOption() => HasValue ? Some(Value!) : None;
     }
