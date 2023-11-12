af513b9c004abc976da571b97690b515a6c4600a
Ruxo Zheng <rz@just.sent.as>
2023-11-12 18:25:30 +0700

    Add string util functions: Left and Right

--- src/RZ.Foundation/Extensions/StringExtension.cs
+++ src/RZ.Foundation/Extensions/StringExtension.cs
@@ -1,18 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using LanguageExt;
 using static LanguageExt.Prelude;
 
-namespace RZ.Foundation.Extensions
+namespace RZ.Foundation.Extensions;
+
+public static class StringExtension
 {
-    public static class StringExtension
-    {
-        public static bool iEquals(this string s, string other) => string.Equals(s, other, StringComparison.OrdinalIgnoreCase);
-        public static string Limit(this string s, int maxLength) => s.Substring(0, Math.Min(Math.Max(0, maxLength), s.Length));
+    public static bool iEquals(this string s, string other) => string.Equals(s, other, StringComparison.OrdinalIgnoreCase);
+    public static string Limit(this string s, int maxLength) => s.Substring(0, Math.Min(Math.Max(0, maxLength), s.Length));
 
-        public static string Join(this IEnumerable<string> sseq, char delimiter) => string.Join(delimiter.ToString(), sseq);
-        public static string Join(this IEnumerable<string> sseq, string delimiter) => string.Join(delimiter, sseq);
+    public static string Join(this IEnumerable<string> sseq, char delimiter) => string.Join(delimiter.ToString(), sseq);
+    public static string Join(this IEnumerable<string> sseq, string delimiter) => string.Join(delimiter, sseq);
 
-        public static Option<DateTime> ToDateTime(this string s) => DateTime.TryParse(s, out var dt) ? Some(dt) : None;
+    public static Option<DateTime> ToDateTime(this string s) => DateTime.TryParse(s, out var dt) ? Some(dt) : None;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Left(this string s, int n) => s[..Math.Min(s.Length, n)];
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string Right(this string s, int n) {
+        return s[Math.Max(0, s.Length - n)..];
     }
 }
---
+++ test/Extensions/StringExtensionTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RZ.Foundation.Extensions;
+
+public sealed class StringExtensionTest
+{
+    [Fact]
+    public void String_Left() {
+        var s = "hello".Left(3);
+
+        s.Should().Be("hel");
+    }
+    
+    [Fact]
+    public void String_Left0() {
+        var s = "hello".Left(0);
+
+        s.Should().Be(string.Empty);
+    }
+    
+    [Fact]
+    public void String_LeftOverflow() {
+        var s = "hello".Left(99);
+
+        s.Should().Be("hello");
+    }
+    
+    [Fact]
+    public void String_Right() {
+        var s = "hello world".Right(3);
+
+        s.Should().Be("rld");
+    }
+    
+    [Fact]
+    public void String_Right0() {
+        var s = "hello world".Right(0);
+
+        s.Should().Be(string.Empty);
+    }
+    
+    [Fact]
+    public void String_RightOverflow() {
+        var s = "hello world".Right(99);
+
+        s.Should().Be("hello world");
+    }
+}
