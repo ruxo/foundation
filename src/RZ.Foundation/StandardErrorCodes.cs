cac42659cebca5cf4aac81966cdd4d30349376a8
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-06-22 09:23:15 +0700

    Introduce ErrorInfo

---
+++ src/RZ.Foundation/StandardErrorCodes.cs
@@ -0,0 +1,28 @@
+namespace RZ.Foundation;
+
+/// <summary>
+/// Standard Error Codes
+/// </summary>
+public static class StandardErrorCodes
+{
+    public const string AuthenticationNeeded = "authenticaton-needed";
+    public const string DatabaseTransactionError = "database-transaction";
+    public const string Duplication = "duplication";
+    public const string HttpError = "http";
+    public const string InvalidRequest = "invalid-request";
+    public const string InvalidResponse = "invalid-response";
+    public const string InvalidOrder = "invalid-order";
+    public const string NetworkError = "network";
+    public const string NotFound = "not-found";
+    public const string PermissionNeeded = "permission-needed";
+    public const string RaceCondition = "race-condition";
+
+    /// <summary>
+    /// Error from subsequent call to another service
+    /// </summary>
+    public const string ServiceError = "service-error";
+
+    public const string Timeout = "timeout";
+    public const string Unhandled = "unhandled";
+    public const string ValidationFailed = "validation-failed";
+}
