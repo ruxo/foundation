1a7cbd305521a3342286cc17a2234af416a1c7ac
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-10-29 09:03:12 +0700

    Introduce Version hash, nav item, and RzRecommendedJsonOptions
    * Add AppVersion.Hash
    * Allow navigation item without path
    * Add recommended JSON option `RzRecommendedJsonOptions`

--- src/RZ.Foundation/AppVersion.cs
+++ src/RZ.Foundation/AppVersion.cs
@@ -6,13 +6,28 @@ namespace RZ.Foundation;
 [PublicAPI]
 public static class AppVersion
 {
-    public static readonly string Current = GetAppVersion(Assembly.GetEntryAssembly()!);
+    public static readonly string Current;
+    public static readonly string Hash;
 
-    public static string GetAppVersion(Assembly assembly) =>
-        Optional(assembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute))
-                     as AssemblyInformationalVersionAttribute)
-           .Map(a => TrimGitHashFromNet8(a.InformationalVersion))
-           .IfNone("(unversioned)");
+    static AppVersion() {
+        var version = GetVersion(Assembly.GetEntryAssembly()!);
+        if (version is null){
+            Current = "(unversioned)";
+            Hash = "";
+        }
+        else {
+            var lastPlus = version.LastIndexOf('+');
+            Current = lastPlus > 0 ? version[..lastPlus] : version;
+            Hash = lastPlus > 0 ? version[(lastPlus + 1)..] : string.Empty;
+        }
+    }
+
+    public static string? GetVersion(Assembly assembly)
+        => (assembly.GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute))
+                as AssemblyInformationalVersionAttribute)?.InformationalVersion;
+
+    public static string GetAppVersion(Assembly assembly)
+        => GetVersion(assembly)?.Apply(TrimGitHashFromNet8) ?? "(unversioned)";
 
     static string TrimGitHashFromNet8(string s) {
         var lastPlus = s.LastIndexOf('+');
