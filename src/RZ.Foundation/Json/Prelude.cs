9b10adbe8dcb6838d671abf4762348ce0ba697ee
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-01-28 19:38:45 +0700

    Add Json serialization helpers

--- src/RZ.Foundation/Json/Prelude.cs
+++ src/RZ.Foundation/Json/Prelude.cs
@@ -1,9 +1,35 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
 using RZ.Foundation.Json;
+using RZ.Foundation.Types;
 
 namespace RZ.Foundation;
 
 public partial class Prelude
 {
     public static readonly JsonSerializerOptions RzRecommendedJsonOptions = new JsonSerializerOptions().UseRzRecommendedSettings();
+
+    #region JSON serialization helpers
+
+    [PublicAPI]
+    public static Outcome<string> JsonSerialize<T>(in T data, JsonSerializerOptions? options = null) where T: notnull {
+        try{
+            return JsonSerializer.Serialize(data, options ?? RzRecommendedJsonOptions);
+        }
+        catch (Exception e){
+            return ErrorFrom.Exception(e);
+        }
+    }
+
+    [PublicAPI]
+    public static Outcome<T> JsonDeserialize<T>(string jsonText, JsonSerializerOptions? options = null) where T : notnull {
+        try{
+            return JsonSerializer.Deserialize<T>(jsonText, options ?? RzRecommendedJsonOptions) is { } result ? result : ErrorInfo.NotFound;
+        }
+        catch (Exception e){
+            return ErrorFrom.Exception(e);
+        }
+    }
+
+    #endregion
 }
