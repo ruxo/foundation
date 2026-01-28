1f8630560b5838fc7a5ff3a34bce79303e593d70
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-01-28 20:00:30 +0700

    Add async Outcome helpers

--- src/RZ.Foundation.AOT/Functional/Outcome/OutcomeExtension.cs
+++ src/RZ.Foundation.AOT/Functional/Outcome/OutcomeExtension.cs
@@ -16,5 +16,29 @@ public static class OutcomeExtension
         [Pure]
         public Outcome<C> SelectMany<B, C>(Func<A, Outcome<B>> bind, Func<A, B, C> project)
             => ma.Bind(x => bind(x).Map(y => project(x, y)));
+
+        [Pure]
+        public async ValueTask<Outcome<C>> SelectMany<B, C>(Func<A, ValueTask<Outcome<B>>> bind, Func<A, B, C> project) {
+            if (ma.IsFail) return ma.Error!;
+            if (Fail(await bind(ma.Data!), out var e, out var result)) return e;
+            return project(ma.Data!, result);
+        }
+    }
+
+    extension<A>(ValueTask<Outcome<A>> ma)
+    {
+        [Pure]
+        public async ValueTask<Outcome<C>> SelectMany<B, C>(Func<A, Outcome<B>> bind, Func<A, B, C> project) {
+            if (Fail(await ma, out var e, out var a)
+             || Fail(bind(a), out e, out var result)) return e;
+            return project(a, result);
+        }
+
+        [Pure]
+        public async ValueTask<Outcome<C>> SelectMany<B, C>(Func<A, ValueTask<Outcome<B>>> bind, Func<A, B, C> project) {
+            if (Fail(await ma, out var e, out var a)
+             || Fail(await bind(a), out e, out var result)) return e;
+            return project(a, result);
+        }
     }
 }
