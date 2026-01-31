3a82ea7348a5c8a0cc4798be4e4ae5d79478245e
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-01-31 18:51:52 +0700

    Add more HTTP extensions

--- src/RZ.Foundation.AOT/Extensions/HttpExtension.cs
+++ src/RZ.Foundation.AOT/Extensions/HttpExtension.cs
@@ -44,20 +44,51 @@ public static class HttpExtension
         }
     }
 
-    [PublicAPI]
-    public static async ValueTask<Outcome<Stream>> ToStream(this HttpResponseMessage r) {
-        using (r)
-            return r.IsSuccessStatusCode
-                       ? Success(await r.Content.ReadStream(), out var v, out var e) ? v : e.Trace("Read stream failed")
-                       : new ErrorInfo(StandardErrorCodes.HttpError, $"({r.StatusCode}) HTTP failed",
-                                       data: ToJson(("StatusCode", r.StatusCode.ToString()),
-                                                    ("ReasonPhrase", r.ReasonPhrase)));
+    extension(HttpResponseMessage r)
+    {
+        async ValueTask<Outcome<T>> Read<T>(Func<HttpContent,ValueTask<Outcome<T>>> reader, string readError) {
+            using (r)
+                return r.IsSuccessStatusCode
+                           ? Success(await reader(r.Content), out var v, out var e) ? v : e.Trace(readError)
+                           : new ErrorInfo(HttpError, $"({r.StatusCode}) HTTP failed",
+                                           data: ToJson(("StatusCode", r.StatusCode.ToString()),
+                                                        ("ReasonPhrase", r.ReasonPhrase)));
+        }
+
+        [PublicAPI]
+        public ValueTask<Outcome<string>> ReadString() => r.Read(c => c.ReadAsString(), "Read string failed");
+
+        [PublicAPI]
+        public ValueTask<Outcome<byte[]>> ReadByteArray() => r.Read(c => c.ReadAsByteArray(), "Read byte array failed");
+
+        [PublicAPI]
+        public ValueTask<Outcome<Stream>> ReadStream() => r.Read(c => c.ReadStream(), "Read stream failed");
     }
 
-    extension(Task<HttpResponseMessage> task)
+    extension(ValueTask<Outcome<HttpResponseMessage>> task)
     {
+        async ValueTask<Outcome<T>> Read<T>(Func<HttpResponseMessage,ValueTask<Outcome<T>>> reader) {
+            try{
+                if (Fail(await task, out var e, out var r)) return e.Trace("Read response failed");
+                using(r)
+                    return await reader(r);
+            }
+            catch (Exception e){
+                return ErrorFrom.Exception(e);
+            }
+        }
+
         [PublicAPI]
-        public async ValueTask<Outcome<Stream>> ToStream()
-            => Fail(await TryCatch(task), out var e, out var r) ? e.Trace("Read response failed") : await r.ToStream();
+        public ValueTask<Outcome<string>> ReadString() => task.Read(r => r.ReadString());
+
+        [PublicAPI]
+        public ValueTask<Outcome<byte[]>> ReadByteArray() => task.Read(r => r.ReadByteArray());
+
+        [PublicAPI]
+        public ValueTask<Outcome<Stream>> ReadStream() => task.Read(r => r.ReadStream());
     }
+
+    [PublicAPI]
+    public static ValueTask<Outcome<Stream>> ReadStream(this Task<HttpResponseMessage> task)
+        => TryCatch(async () => await task).ReadStream();
 }
