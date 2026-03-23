55088a9c5bbd413d31e71111c99d6def7eb395fc
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-03-24 05:53:52 +0700

    Introduce ActivityId

---
+++ src/RZ.Foundation.AOT/Types/ActivityId.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace RZ.Foundation.Types;
+
+/// <summary>
+/// Represents a traceable activity identifier (mostly for OpenTelemetry).
+/// </summary>
+[PublicAPI]
+public readonly record struct ActivityId(string Value)
+{
+    /// <summary>
+    /// Get current activity id.
+    /// </summary>
+    public static ActivityId? Current => Activity.Current?.Id is {} v? new(v) : null;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static ActivityId FromString(string value) => new(value);
+}
