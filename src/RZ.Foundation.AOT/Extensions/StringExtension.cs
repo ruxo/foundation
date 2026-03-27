741886bc4752beecb7e148288946fd1112a4b4be
Ruxo Zheng <ruxo@users.noreply.github.com>
2026-03-27 19:52:11 +0700

    Add Truncate and TruncateRight and test cases

--- src/RZ.Foundation.AOT/Extensions/StringExtension.cs
+++ src/RZ.Foundation.AOT/Extensions/StringExtension.cs
@@ -10,13 +10,22 @@ public static class StringExtension
         public bool iEquals(string other) => string.Equals(s, other, StringComparison.OrdinalIgnoreCase);
 
         [PublicAPI, Pure]
-        public string Limit(int maxLength) => s.Substring(0, Math.Min(Math.Max(0, maxLength), s.Length));
+        public string Limit(int maxLength)
+            => s[..Math.Min(Math.Max(0, maxLength), s.Length)];
+
+        [PublicAPI, Pure]
+        public string Truncate(int n)
+            => n < 1 ? string.Empty : s.Length <= n ? s : string.Concat(s.AsSpan(0, n - 1), "…");
+
+        [PublicAPI, Pure]
+        public string TruncateRight(int n)
+        => n <= 0? string.Empty : n >= s.Length? s : string.Concat("…", s.AsSpan(s.Length - n + 1));
 
         [PublicAPI, Pure]
         public Option<DateTime> ToDateTime() => DateTime.TryParse(s, out var dt) ? dt : None;
 
         [PublicAPI, Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public string Left(int n) => s[..Math.Min(s.Length, n)];
+        public string Left(int n) => s.Limit(n);
 
         [PublicAPI, Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string Right(int n) {
